1fb7383bb8ae85f23c99126c969d4fc11dcaa1e4
siman <simanmore@yandex.ru>
2022-03-31 15:07:20 +0700

    init

---
+++ internal/api/handlers/handlers.go
@@ -0,0 +1,9 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (app *api.App) searchInGroups(c *gin.Context) {
+
+}
---
+++ internal/model/request.go
@@ -0,0 +1,6 @@
+package model
+
+type Request struct {
+	Text  string
+	Links []string
+}
